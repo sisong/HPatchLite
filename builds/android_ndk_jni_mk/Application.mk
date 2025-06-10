f25b90dc53deeaaaf28919c43ecb390e5a3b66b9
sisong <sisong@gmail.com>
2025-06-10 19:57:47 +0800

    fix NDK ci

--- builds/android_ndk_jni_mk/Application.mk
+++ builds/android_ndk_jni_mk/Application.mk
@@ -5,4 +5,4 @@ APP_CFLAGS += -ffunction-sections -fdata-sections
 APP_LDFLAGS += -Wl,--gc-sections
 APP_LDFLAGS += -Wl,-z,max-page-size=16384
 APP_BUILD_SCRIPT := Android.mk
-APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
